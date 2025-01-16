6bdda804f70097d4a8562d09d5133c04a9e9a958
Sunil Poudel <suneelpdl90@gmail.com>
2025-01-16 16:34:14 +0545

    actuators

--- src/main/java/org/sunil_spring_demo/spring_001/rest/RestControllers.java
+++ src/main/java/org/sunil_spring_demo/spring_001/rest/RestControllers.java
@@ -1,7 +1,6 @@
 package org.sunil_spring_demo.spring_001.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +9,10 @@ public class RestControllers {
     @GetMapping("/")
     public String hello(){
         return "Hello World!";
+    }
+
+    @GetMapping("/about-me")
+    public String about(){
+        return "My name is Sunil Poudel. I am from Puranchaur!";
     }
 }
