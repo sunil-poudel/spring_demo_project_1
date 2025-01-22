bd08f0b05eedd153f41ceef426867b278d7b102d
Sunil Poudel <suneelpdl90@gmail.com>
2025-01-22 20:00:18 +0545

    complete initial steps

--- src/main/java/org/sunil_spring_demo/spring_001/rest/RestControllers.java
+++ src/main/java/org/sunil_spring_demo/spring_001/rest/RestControllers.java
@@ -1,10 +1,21 @@
 package org.sunil_spring_demo.spring_001.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestControllers {
+
+    @Value("${city.name}")
+    private String cityName;
+    @Value("${district.name}")
+    private String districtName;
+
+    @GetMapping("/about-pokhara")
+    public String aboutPokhara(){
+        return "city: "+cityName+", district: "+districtName;
+    }
 
     @GetMapping("/")
     public String hello(){
